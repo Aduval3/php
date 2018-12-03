f53a91376d6f3158bf16a3cd1a7a7a5c4b0c2998
Aduval3 <45578519+Aduval3@users.noreply.github.com>
2018-12-03 18:57:50 -0500

    Add files via upload

---
+++ PHP/hello-function.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title> Hello Function </title>
+</head>
+
+<body>
+<h1> Hello</h1>
+
+<?php
+
+function hello(){
+  echo "Hello, World!";
+}
+//Call function.
+hello();
+
+?>
+</body>
+</html>
